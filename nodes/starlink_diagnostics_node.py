4e83a330e39f433f828208b4c69d0236e10462ba
Field User <field@ccom.unh.edu>
2024-03-28 17:05:15 +0000

    adds catch for malformed response causing node to crash

--- nodes/starlink_diagnostics_node.py
+++ nodes/starlink_diagnostics_node.py
@@ -26,32 +26,37 @@ def publish_diagnostic_message():
         # Create diagnostic array message
         diag_array = DiagnosticArray()
         diag_array.header.stamp = rospy.Time.now()
-        diagnostic_data = get_diagnostics("192.168.100.1")
 
-        # Create diagnostic status message for each key-value pair in dictionary
-        for key, value in diagnostic_data.items():
-            # Create diagnostic status message and set its values
-            diag_status = DiagnosticStatus()
-            diag_status.name = 'Starlink'
-            diag_status.hardware_id = (value.get('device_info',{}).get('id'))
-            diag_status.message = value.get('message', '')
-            diag_status.level = value.get('level', DiagnosticStatus.OK)
-            diag_status.values = []
-            value = flatten(value)
-            # Create key-value messages for each item in the subdictionary
-            for k, v in value.items():
-                if k != 'level' and k != 'message':
-                    key_value = KeyValue()
-                    key_value.key = k
-                    key_value.value = str(v)
-                    diag_status.values.append(key_value)
+        try:
+            diagnostic_data = get_diagnostics("192.168.100.1")
 
-            # Add diagnostic status message to diagnostic array
-            diag_array.status.append(diag_status)
+            # Create diagnostic status message for each key-value pair in dictionary
+            for key, value in diagnostic_data.items():
+                # Create diagnostic status message and set its values
+                diag_status = DiagnosticStatus()
+                diag_status.name = 'Starlink'
+                diag_status.hardware_id = (value.get('device_info',{}).get('id'))
+                diag_status.message = value.get('message', '')
+                diag_status.level = value.get('level', DiagnosticStatus.OK)
+                diag_status.values = []
+                value = flatten(value)
+                # Create key-value messages for each item in the subdictionary
+                for k, v in value.items():
+                    if k != 'level' and k != 'message':
+                        key_value = KeyValue()
+                        key_value.key = k
+                        key_value.value = str(v)
+                        diag_status.values.append(key_value)
 
-        # Publish diagnostic array message
-        diag_array.header.stamp = rospy.Time.now()
-        pub.publish(diag_array)
+                # Add diagnostic status message to diagnostic array
+                diag_array.status.append(diag_status)
+
+            # Publish diagnostic array message
+            diag_array.header.stamp = rospy.Time.now()
+            pub.publish(diag_array)
+        except:
+            rospy.loginfo("Connection to starlink dish failed. Re-trying... ")
+
         rospy.sleep(1)
 
 if __name__ == '__main__':
